4a34f32dfeeb879d0c8ab4f4598ad7530cac3d52
julieSalha <julie.salha@gmail.com>
2021-03-18 16:11:27 +0100

    update

--- code.js
+++ code.js
@@ -3,12 +3,13 @@
 // Exemple  2021-11-15T10:00:00Z
 
 // Get time
-let countDownDate = new Date('2051-03-18T11:30:00').getTime();
+let countDownDate = new Date('2021-03-28T11:30:00').getTime();
 
 // Update countdown every second
 let count = setInterval(countDown, 1000);
 
 function countDown() {
+    console.log('count');
     // Milliseconds
     let now             = new Date().getTime();
     let duration        = countDownDate - now;
@@ -22,11 +23,16 @@ function countDown() {
     let hours           = Math.floor((duration / (1000 * 60 * 60) ) % 24 );
     // days
     let days            = Math.floor((duration / (1000 * 60 * 60 * 24)));
-    document.querySelector('#app').innerHTML = days + 'days' + hours + 'hours' + minutes + 'minutes' + seconds + 'seconds';
+
+    document.querySelector('#app').innerHTML = days + ' j, ' + hours + ' h,' + minutes + ' m,' + seconds + ' s';
 
     if (duration < 0) {
-        console.log('distance < 0');
-        document.querySelector('#app').innerHTML = 'END COUNT DOWN';
+        document.querySelector('#app').innerHTML = '0 j, 0, h, 0, m, 0 s';
+        clearInterval(count);
     }
 }
+
+
+
+
 
